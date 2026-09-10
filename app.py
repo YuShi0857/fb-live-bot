c86bf717204162be37e4dc19a2e3ee401d1b1088
YuShi0857 <plinadd0001@gmail.com>
2026-09-10 22:50:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ generation_config = {
     "temperature": 0.9, 
     "top_p": 0.95,
 }
-model = genai.GenerativeModel('gemini-3.6-flash', generation_config=generation_config)
+model = genai.GenerativeModel('gemini-1.5-flash', generation_config=generation_config)
 
 # ==========================================
 # 初始化歷史紀錄的暫存空間
